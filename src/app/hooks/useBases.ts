cc55348eb448e0c1bbef35bc0071574473751673
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2025-05-23 17:46:13 +1000

    add useBases hook

---
+++ src/app/hooks/useBases.ts
@@ -0,0 +1,57 @@
+import { useCallback } from 'react'
+import type { Base } from '~/schemas'
+import { api } from '~/trpc/react'
+
+export function useBases(
+  userId: string
+) {
+  const {
+    data: bases,
+    isLoading,
+    refetch: refetchBases,
+  } = api.base.getBases.useQuery({ userId })
+
+  const {
+    mutate: addMutate,
+    status: addBaseStatus,
+  } = api.base.addBase.useMutation({ onSuccess: () => refetchBases() })
+
+  const {
+    mutate: updateMutate,
+    status: updBaseStatus,
+  } = api.base.updBase.useMutation({ onSuccess: () => refetchBases() })
+
+  const {
+    mutate: deleteMutate,
+    status: delBaseStatus,
+  } = api.base.delBase.useMutation({ onSuccess: () => refetchBases() })
+
+  const addBase = useCallback((name: string) => {
+    addMutate({
+      userId,
+      name,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString()
+    })
+  }, [userId, addMutate])
+
+  const updateBase = useCallback((base: Base) => {
+    updateMutate({ userId, base })
+  }, [userId, updateMutate])
+
+  const deleteBase = useCallback((baseId: number) => {
+    deleteMutate({ userId, baseId })
+  }, [userId, deleteMutate])
+
+  return {
+    bases,
+    isLoading,
+    addBaseStatus,
+    updBaseStatus,
+    delBaseStatus,
+    addBase,
+    updateBase,
+    deleteBase,
+    refetchBases,
+  }
+}
